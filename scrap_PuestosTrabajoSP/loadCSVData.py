d5cc1b70860ac7bd30d26f969f320824af9b2b1c
Zalazar Matias <matizalazar2001@gmail.com>
2023-06-19 19:30:12 -0300

    Anda

--- scrap_PuestosTrabajoSP/loadCSVData.py
+++ scrap_PuestosTrabajoSP/loadCSVData.py
@@ -12,7 +12,7 @@ class LoadCSVData:
         conn = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='Estadistica123',
+            password='',
             database='prueba1'
         )
 
@@ -23,20 +23,16 @@ class LoadCSVData:
         df = pd.read_csv(file_path)  # Leer el archivo CSV y crear el DataFrame
         df = df.replace({np.nan: None})  # Reemplazar los valores NaN(Not a Number) por None
 
-        # Verificar si la tabla ya contiene registros
-        select_query = f"SELECT fecha, codigo_departamento_indec, id_provincia_indec, clae2, puestos FROM {table_name}"
-        existing_records = pd.read_sql(select_query, conn)
-
-        # Filtrar los registros del archivo CSV que no se encuentran en la tabla
-        df_to_insert = df[~df.set_index(existing_records.columns).index.isin(existing_records.set_index(existing_records.columns).index)]
+        print("columnas -- ", df.columns)
 
         # Aplicar strip() al nombre de la columna antes de acceder a ella
         column_name = ' puestos '  # Nombre de la columna con espacios en blanco
         column_name_stripped = column_name.strip()  # Eliminar espacios en blanco
+
         # Verificar si la columna existe en el DataFrame
         if column_name_stripped in df.columns:
             # Realizar transformaciones en el DataFrame utilizando el nombre de columna sin espacios
-            df_to_insert.loc[df_to_insert[column_name_stripped] < 0, column_name_stripped] = 0
+            df.loc[df[column_name_stripped] < 0, column_name_stripped] = 0 #Los datos <0 se reempalazan a 0
         else:
             print(f"La columna '{column_name_stripped}' no existe en el DataFrame.")
 
@@ -46,16 +42,17 @@ class LoadCSVData:
 
         #Representa la consulta SQL de inserción en la tabla de la base de datos. 
         insert_query = f"INSERT INTO {table_name} VALUES ("
-        for index, row in df_to_insert.iterrows():
-            # Se crea una cadena values que contiene marcadores de posición %s para cada valor en una fila.
+        for index, row in df.iterrows(): #Se itera sobre las filas del DataFrame
+            print("en el for:")
+            #Se crea una cadena values que contiene marcadores de posición %s para cada valor en una fila.
             values = ', '.join(["%s" for _ in range(len(row))])
-            # Se convierte la fila en una tupla de valores, que se utilizará como argumento en la consulta de inserción.
+            #Se convierte la fila en una tupla de valores, que se utilizará como argumento en la consulta de inserción.
             data_tuple = tuple(row.values)
-            # Se ejecuta la consulta de inserción utilizando el objeto cursor
+            #Se ejecuta la consulta de inserción utilizando el objeto cursor//Se concatenan la consulta insert_query, los valores values y un paréntesis de cierre
+            #La tupla data_tuple se pasa como argumento para proporcionar los valores a insertar en la tabla.
             conn.cursor().execute(insert_query + values + ")", data_tuple)
             print("values")
             print(data_tuple)
-
         conn.commit()
         
         print("GUARDO!!!!!")
@@ -66,4 +63,3 @@ class LoadCSVData:
         
         # Cerrar la conexión a la base de datos
         conn.close()
-    
