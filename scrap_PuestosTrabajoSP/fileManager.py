215cf8f4785884bf1459b313fa6cddad03f719ab
Zallmatt <matizalazar2001@gmail.com>
2023-07-11 08:49:49 -0300

    Ultima ruta arreglada

--- scrap_PuestosTrabajoSP/fileManager.py
+++ scrap_PuestosTrabajoSP/fileManager.py
@@ -7,7 +7,7 @@ from pathlib import Path
 class FileManager:
     
     url = ""
-    folder = str(Path.cwd()) + "/scrap_PuestosTrabajoSP/files/csv"#path.cdw toma la ruta actual de trabajo//Se le agrega la carpeta en donde se va a guardad
+    folder = str(Path.cwd()) + "/scrap_PuestosTrabajoSP/files"#path.cdw toma la ruta actual de trabajo//Se le agrega la carpeta en donde se va a guardad
 
     def __init__(self, url): #Inicializar los atributos del objeto que creamos.
         self.url = url #Esto permite proporcionar la URL al crear una instancia de la clase.
@@ -20,9 +20,8 @@ class FileManager:
 
         if response.status_code == 200:#Se verifica que el codigo sea de exito
             csv_content = response.content #Si es exitoso se guarda el contenido en la variable
-            timestamp = str(datetime.now().timestamp()).replace(".", "_")#Se genera una marca de tiempo actual en forma de cadena
-            file_name = timestamp + ".csv" #Se crea el nombre
-            file_path = os.path.join(self.folder, file_name)#Crea la ruta del archivo el path.join une el folder(L10) y el file name creado arriba
+            file_name = "trabajoSectorPrivado.csv"  # Nombre personalizado del archivo
+            file_path = os.path.join(self.folder, file_name)
 
             with open(file_path, "wb") as file:#with open lo abre en escritura binaria//El parámetro file_path especifica la ruta completa del archivo que se desea abrir y crear. 
                 file.write(csv_content)#Escribe los datos en el archivo
--- scrap_PuestosTrabajoSP/main.py
+++ scrap_PuestosTrabajoSP/main.py
@@ -2,15 +2,23 @@ from homePage import HomePage
 from fileManager import FileManager
 from loadCSVData import LoadCSVData
 from loadCSVDataDepartamentos import LoadCSVDataDepartamentos
+import os
 
 if __name__ == '__main__':
    url =  HomePage().getDownloadUrl()
    file_name = FileManager(url).downloadCSV()
-   print("---->", file_name)
-   #file_path_Departamentos="D:\\Users\\Pc-Pix211\\Desktop\\scrapingTrabajo\\scrap_PuestosTrabajoSP\\files\\csv\\diccionario_cod_depto.csv"
-   #LoadCSVDataDepartamentos().loadInDataBase(file_path_Departamentos)
-   file_path="C:\\Users\\Usuario\\Desktop\\scrapingTrabajo\\scrap_PuestosTrabajoSP\\files\\csv\\{}".format(file_name)
+   # Obtener la ruta del directorio actual (donde se encuentra el script)
+   directorio_actual = os.path.dirname(os.path.abspath(__file__))
+
+   # Construir la ruta de la carpeta "files" dentro del directorio actual
+   ruta_carpeta_files = os.path.join(directorio_actual, 'files')
+
+   # Construir la ruta completa del archivo CSV dentro de la carpeta "files"
+   file_path = os.path.join(ruta_carpeta_files, file_name)
    print("---->", file_path)
+   
    LoadCSVData().loadInDataBase(file_path)
    
-   
+
+   #file_path_Departamentos="D:\\Users\\Pc-Pix211\\Desktop\\scrapingTrabajo\\scrap_PuestosTrabajoSP\\files\\csv\\diccionario_cod_depto.csv"
+   #LoadCSVDataDepartamentos().loadInDataBase(file_path_Departamentos)
