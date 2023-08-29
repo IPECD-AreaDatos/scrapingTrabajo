d65d42cb8d3b993e1369c88ae80ea56f4a8a515f
Zallmatt <matizalazar2001@gmail.com>
2023-08-29 11:59:16 -0300

    Ipc_productos

--- scrap_IPC/armadoXLSProductos.py
+++ scrap_IPC/armadoXLSProductos.py
@@ -8,46 +8,77 @@ class LoadXLSDataProductos:
     def loadInDataBase(self, file_path_productos):
         # Se toma el tiempo de comienzo
         start_time = time.time()
-
-        
         
         try:
             # Listas para almacenar los datos
-            datos_region = []
-            datos_productos = []
-            fechas = []
-            valores = []
+            region = []
+            producto = []
+            fecha = []
+            valor = []
             fecha_inicial = datetime.date(2017, 6, 1)
-            
+
+            # Abrir el archivo de Excel
             workbook = xlrd.open_workbook(file_path_productos)
-            
+
             # Seleccionar la primera hoja
             sheet = workbook.sheet_by_index(0)
-            
+
+            # Leer los datos de las filas
             for fila in range(6, min(90, sheet.nrows)):
-                region = sheet.cell_value(fila, 0)  # Obtener dato de la primera columna
-                productos = sheet.cell_value(fila, 1)  # Obtener dato de la segunda columna
-                
-                # Obtener datos de la fila (excluyendo espacios en blanco)
+                # Obtener la región
+                region.append(sheet.cell_value(fila, 0))
+
+                # Obtener el producto
+                producto.append(sheet.cell_value(fila, 1))
+
+                # Obtener los datos de la fila
                 datos_fila = sheet.row_values(fila)[3:]
                 datos_fila = [valor for valor in datos_fila if valor != ""]
-                
+
+                # Agregar los datos a las listas
                 if datos_fila:
-                    datos_region.append(region)
-                    datos_productos.append(productos)
-                    fechas.append(fecha_inicial.strftime('%Y-%m-%d'))  # Convertir la fecha en formato string
-                    valores.append(datos_fila)
-                    
-                    # Incrementar la fecha para el siguiente ciclo
-                    fecha_inicial = fecha_inicial + relativedelta(months=1)  # Avanzar un mes
+                    fecha.append(fecha_inicial.strftime('%Y-%m-%d'))
+                    valor.append(datos_fila)
+
+                    # Actualizar la fecha para el siguiente ciclo
+                    fecha_inicial = fecha_inicial + relativedelta(months=1)
+
+            # Crear un diccionario con los datos
+            data = {
+                'ID_Region': region,
+                'Fecha': fecha,
+                'Producto': producto,
+                'Valor': valor,
+            }
 
             # Crear un DataFrame con los datos
-            data = {'ID_Region': datos_region, 'Producto': datos_productos, 'Fecha': fechas, 'Valor': valores}
             df = pd.DataFrame(data)
 
-            print(df)
+            # Ajustar los tipos de datos
+            df['ID_Region'] = df['ID_Region'].astype(int)
+            df['Fecha'] = pd.to_datetime(df['Fecha'])
+            df['Valor'] = df['Valor'].astype(float)
 
-            print(datos_fila)
+            # Transformar las regiones a ID
+            regiones = {
+                "Nacion": 1,
+                "GBA": 2,
+                "Pampeana": 3,
+                "NOA": 4,
+                "NEA": 5,
+                "Cuyo": 6,
+                "Patagonia": 7,
+            }
+
+            for i in range(len(df)):
+                region = df.loc[i, 'ID_Region']
+                for palabra in ["Nación", "GBA", "Pampeana", "NOA", "NEA", "Cuyo", "Patagonia"]:
+                    if region == palabra:
+                        df.loc[i, 'ID_Region'] = regiones[palabra]
+
+            # Guardar el DataFrame en un archivo CSV
+            df.to_csv('ipc_productos.csv', index=False)
+            
         except Exception as e:
             # Manejar cualquier excepción ocurrida durante la carga de datos
             print(f"Data Cuyo: Ocurrió un error durante la carga de datos: {str(e)}")
