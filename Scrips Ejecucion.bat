4c341ea1aefaae82c1f98cabf40fa2beea586408
Zallmatt <matizalazar2001@gmail.com>
2023-10-12 10:56:21 -0300

    Se agrego el ejecutable de todos los codigos

---
+++ Scrips Ejecucion.bat
@@ -0,0 +1,70 @@
+@echo off 
+
+echo Comenzando la ejecucion
+title Programa datos
+
+echo ------------------------------------
+echo RIPTE
+echo ------------------------------------
+python C:\Users\Usuario\Desktop\scrapingTrabajo\scarp_RIPTE\main.py
+echo ------------------------------------
+echo Termino RIPTE
+echo ------------------------------------
+
+echo ------------------------------------
+echo EMAE
+echo ------------------------------------
+python C:\Users\Usuario\Desktop\scrapingTrabajo\scrap_EMAE\main.py
+echo ------------------------------------
+echo Termino EMAE
+echo ------------------------------------
+
+echo ------------------------------------
+echo SIPA
+echo ------------------------------------
+python C:\Users\Usuario\Desktop\scrapingTrabajo\scrap_SIPA\main.py
+echo ------------------------------------
+echo Termino SIPA
+echo ------------------------------------
+
+echo ------------------------------------
+echo IPC
+echo ------------------------------------
+python C:\Users\Usuario\Desktop\scrapingTrabajo\scrap_IPC\main.py
+echo ------------------------------------
+echo Termino IPC
+echo ------------------------------------
+
+echo ------------------------------------
+echo PUESTOS TRABAJO
+echo ------------------------------------
+python C:\Users\Usuario\Desktop\scrapingTrabajo\scrap_PuestosTrabajoSP\main.py
+echo ------------------------------------
+echo Termino PUESTOS TRABAJO
+echo ------------------------------------
+
+echo ------------------------------------
+echo SALARIO MINIMO VITAL Y MOVIL
+echo ------------------------------------
+python C:\Users\Usuario\Desktop\scrapingTrabajo\scrap_SalarioMVM\main.py
+echo ------------------------------------
+echo Termino SALARIO MINIMO VITAL Y MOVIL
+echo ------------------------------------
+
+echo ------------------------------------
+echo SALARIO SECTOR PRIVADO Y TOTAL
+echo ------------------------------------
+python C:\Users\Usuario\Desktop\scrapingTrabajo\scrap_SalarioSP-Total\main.py
+echo ------------------------------------
+echo Termino SALARIO SECTOR PRIVADO Y TOTAL
+echo ------------------------------------
+
+echo ------------------------------------
+echo DNRPA
+echo ------------------------------------
+python C:\Users\Usuario\Desktop\scrapingTrabajo\scrap_DNRPA\main.py
+echo ------------------------------------
+echo Termino DNRPA
+echo ------------------------------------
+
+echo Termino la ejecucion
