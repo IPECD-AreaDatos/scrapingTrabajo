14c4b845e681eeb4e5fa0071d1dcf1a539025b37
Zallmatt <matizalazar2001@gmail.com>
2023-10-23 10:39:41 -0300

    Optimizacion ejecucion Salarios y Trabajo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # scrapingTrabajo
+
+
+Base de pruebas
+host = '172.17.16.166'
+user = 'Ivan'
+password = 'Estadistica123'
+database = 'prueba1'
--- scrap_SalarioSP-Total/loadCSVData_Total.py
+++ scrap_SalarioSP-Total/loadCSVData_Total.py
@@ -20,90 +20,53 @@ class loadCSVData_Total:
         )
         cursor = conn.cursor()
         
-        # Nombre de la tabla en MySQL
         table_name = 'DP_salarios_total'
-        # Obtener la ruta del directorio actual (donde se encuentra el script)
         directorio_actual = os.path.dirname(os.path.abspath(__file__))
-        # Construir la ruta de la carpeta "files" dentro del directorio actual
         ruta_carpeta_files = os.path.join(directorio_actual, 'files')
         file_name = "salarioPromedioTotal.csv"
-        # Construir la ruta completa del archivo CSV dentro de la carpeta "files"
         file_path = os.path.join(ruta_carpeta_files, file_name)
                 
         # Leer el archivo de csv y hacer transformaciones
-        df = pd.read_csv(file_path)  # Leer el archivo CSV y crear el DataFrame
+        df = pd.read_csv(file_path) 
         df = df.replace({np.nan: None})  # Reemplazar los valores NaN(Not a Number) por None
         
-        longitud = len(df)
-        print("total: ", longitud)
+        longitud_datos_excel = len(df)
+        print("Salarios Total: ", longitud_datos_excel)
         
         select_row_count_query = "SELECT COUNT(*) FROM DP_salarios_total"
         cursor.execute(select_row_count_query)
-        row_count_before = cursor.fetchone()[0]
-        print("Base: ", row_count_before)
+        filas_BD = cursor.fetchone()[0]
+        print("Base de salarios total: ", filas_BD)
         
+        if longitud_datos_excel != filas_BD:
+            df_datos_nuevos = df.tail(longitud_datos_excel - filas_BD)
 
-        exit()
-        print("columnas -- ", df.columns)
+            column_name = ' w_mean ' 
+            column_name_stripped = column_name.strip() 
 
-        # Aplicar strip() al nombre de la columna antes de acceder a ella
-        column_name = ' w_mean '  # Nombre de la columna con espacios en blanco
-        column_name_stripped = column_name.strip()  # Eliminar espacios en blanco
-
-        # Verificar si la columna existe en el DataFrame
-        if column_name_stripped in df.columns:
-            # Realizar transformaciones en el DataFrame utilizando el nombre de columna sin espacios
-            df.loc[df[column_name_stripped] < 0, column_name_stripped] = 0 #Los datos <0 se reempalazan a 0
-        else:
-            print(f"La columna '{column_name_stripped}' no existe en el DataFrame.")
-
-
-        #↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓CARGA EN BASE DE DATOS ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-        print("Tabla de Salarios Total")
-        select_row_count_query = "SELECT COUNT(*) FROM DP_salarios_total"
-        cursor.execute(select_row_count_query)
-        row_count_before = cursor.fetchone()[0]
-        
-        delete_query ="TRUNCATE `ipecd_economico`.`DP_salarios_total`"
-        cursor.execute(delete_query)
-        
-        insert_query = f"INSERT INTO {table_name} VALUES ({', '.join(['%s' for _ in range(len(df.columns))])})"
-
-
-        for index, row in df.iterrows():
-            data_tuple = tuple(row)
-            # Si los valores no existen, realizar la inserción
-            conn.cursor().execute(insert_query, data_tuple)
-            print(data_tuple)
+            # Verificar si la columna existe en el DataFrame
+            if column_name_stripped in df_datos_nuevos.columns:
+                df_datos_nuevos.loc[df_datos_nuevos[column_name_stripped] < 0, column_name_stripped] = 0 #Los datos <0 se reempalazan a 0
+            else:
+                print(f"La columna '{column_name_stripped}' no existe en el DataFrame.")
             
-            # Agregar los datos nuevos a la lista
-            nuevos_datos.append(data_tuple)
-
-        cursor.execute(select_row_count_query)
-        row_count_after = cursor.fetchone()[0]
-        
-        #Comparar la cantidad de antes y despues
-        if row_count_after > row_count_before:
+            print("Tabla de Salarios Total")
+            insert_query = f"INSERT INTO {table_name} VALUES ({', '.join(['%s' for _ in range(len(df_datos_nuevos.columns))])})"
+            for row in df_datos_nuevos.iterrows():
+                data_tuple = tuple(row)
+                conn.cursor().execute(insert_query, data_tuple)
+                print(data_tuple)
+                nuevos_datos.append(data_tuple)
+            conn.commit()
+            conn.close()
             print("Se agregaron nuevos datos")
             enviar_correo()   
         else:
             print("Se realizo una verificacion de la base de datos")
 
-        conn.commit()
-        # Cerrar la conexión a la base de datos
-        conn.close()
-        
-        
         print("==========================================================")
         print("Se realizo la actualizacion de la tabla de Salarios Total")
         print("==========================================================")
-        
-        #Se toma el tiempo de finalizacion y se calcula
-        end_time = time.time()
-        duration = end_time - start_time
-        print(f"Tiempo de ejecución: {duration} segundos")
-        
-
         
         
 def enviar_correo():
