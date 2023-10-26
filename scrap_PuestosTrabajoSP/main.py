80325740188344011af50395670b5d1aa4352919
Zallmatt <matizalazar2001@gmail.com>
2023-10-26 12:21:37 -0300

    IPICORR Comienzo

--- scrap_PuestosTrabajoSP/main.py
+++ scrap_PuestosTrabajoSP/main.py
@@ -1,4 +1,4 @@
-#from homePage import HomePage
+from homePage import HomePage
 from loadCSVData import LoadCSVData
 from loadCSVPuestosTotal import LoadCSVDataPuestosTotal
 
@@ -9,7 +9,7 @@ password = 'HCj_BmbCtTuCv5}'
 database = 'ipecd_economico'
 
 if __name__ == '__main__':
-    #home_page = HomePage()
-    #home_page.descargar_archivo()
+    home_page = HomePage()
+    home_page.descargar_archivo()
     LoadCSVData().loadInDataBase(host, user, password, database)
     LoadCSVDataPuestosTotal().loadInDataBase(host, user, password, database)
