d712dddc22a633978f421c019d8fad6509604015
Zallmatt <matizalazar2001@gmail.com>
2023-10-27 12:13:58 -0300

    IPICORR

--- scrap_IPICORR/readSheets.py
+++ scrap_IPICORR/readSheets.py
@@ -1,36 +1,27 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import pandas as pd
-import gspread
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
-class read:
-    def sheets(self):
-        driver = webdriver.Chrome()
-        # URL de la página que deseas obtener
-        url_pagina = 'https://docs.google.com/spreadsheets/d/1NGcF5fXO7RCXIRGJ2UQO98x_T_tZtwHTnvD-RmTdV0E/edit?usp=sharing'
+# Define los alcances y la ruta al archivo JSON de credenciales
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+KEY = 'C:\\Users\\Usuario\\Desktop\\scrapingTrabajo\\scrap_IPICORR\\files\\key.json'  # Asegúrate de usar doble barra invertida en la ruta
 
-        # Abre la página
-        driver.get(url_pagina)
+# Escribe aquí el ID de tu documento:
+SPREADSHEET_ID = '1NGcF5fXO7RCXIRGJ2UQO98x_T_tZtwHTnvD-RmTdV0E'
 
-        # Espera a que un elemento específico se cargue (puedes cambiar 'element_id' por el ID del elemento que deseas esperar)
-        element_id = 'docs-menubar'
-        wait = WebDriverWait(driver, 10)  # 10 segundos de tiempo de espera (ajusta según sea necesario)
-        wait.until(EC.presence_of_element_located((By.ID, element_id)))
-        
-        # Descargar los datos de Google Sheets usando gspread y pandas
-        gc = gspread.service_account()
-        worksheet = gc.open_by_url(url_pagina).sheet1  # Hoja 1 de la hoja de cálculo
-        values = worksheet.get_all_values()
+# Carga las credenciales desde el archivo JSON
+creds = service_account.Credentials.from_service_account_file(KEY, scopes=SCOPES)
 
-        # Crea un DataFrame de Pandas a partir de los valores
-        df = pd.DataFrame(values)
+# Crea una instancia de la API de Google Sheets
+service = build('sheets', 'v4', credentials=creds)
+sheet = service.spreadsheets()
 
-        # Opcional: Establece la primera fila como encabezado
-        df.columns = df.iloc[0]
-        df = df[1:]
-        print(df)
-        
-read().sheets()
-                
+# Realiza una llamada a la API para obtener datos desde la hoja 'Hoja 1' en el rango 'A1:A8'
+result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range='Datos para tablero').execute()
+
+# Extrae los valores del resultado
+values = result.get('values', [])
+
+# Imprime los valores
+for row in values:
+    print(row)
