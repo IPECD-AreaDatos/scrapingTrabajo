6477e2c6e3686b2ec4459a109fa8ae9d519cea1a
gastonbenitez2001 <gastongrillo2001@gmail.com>
2023-12-11 09:55:25 -0300

    credenciales IPi

--- scrap_IPICORR/main.py
+++ scrap_IPICORR/main.py
@@ -2,6 +2,9 @@ from readSheets import readSheets
 from connect_db import connect_db
 from homePage_IPI import HomePage_IPI
 from database_ipi import Database_ipi
+import os
+import sys
+
 #Datos de la base de datos
 host = '172.17.22.23'
 user = 'team-datos'
@@ -9,15 +12,31 @@ password = 'HCj_BmbCtTuCv5}'
 database = 'ipecd_economico'
 
 
+
+# Obtener la ruta al directorio actual del script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+credenciales_dir = os.path.join(script_dir, '..', 'Credenciales_folder')
+# Agregar la ruta al sys.path
+sys.path.append(credenciales_dir)
+
+from credenciales_bdd import Credenciales
+
+credenciales = Credenciales()
+
+# Crea una instancia de la clase "Credenciales"
+instancia_credenciales = Credenciales()
+
+
+
 if __name__ == "__main__":
     df = readSheets().tratar_datos()
     print(df)
-    connect_db().connect(df, host, user, password, database)
+    connect_db().connect(df, credenciales.host, credenciales.user, credenciales.password, credenciales.database)
 
  
     home_page = HomePage_IPI()
     df_ipi = home_page.construir_df()
 
     
-    Database_ipi().cargar_datos(host, user, password, database,df_ipi)
+    Database_ipi().cargar_datos(credenciales.host, credenciales.user, credenciales.password, credenciales.database,df_ipi)
 
