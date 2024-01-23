10e504f05dcd80eb310ec1f89dd3d383976c3827
gastonbenitez2001 <gastongrillo2001@gmail.com>
2024-01-23 08:31:55 -0300

    borrado de funciones no necesarias

--- scrap_EMAE/cargaIndice.py
+++ scrap_EMAE/cargaIndice.py
@@ -1,13 +1,9 @@
 import numpy as np
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime
 import mysql
 import mysql.connector
 from dateutil.relativedelta import relativedelta
-from email.message import EmailMessage
-import ssl
-import smtplib
-import calendar
 from informes import InformesEmae
 
 class cargaIndice:
@@ -37,29 +33,15 @@ class cargaIndice:
         # Eliminar la última fila que contiene "Fuente: INDEC"
         df = df.drop(df.index[-1])
         df = df.drop(df.index[-1])
-        
-
-        print(" \n DATAFRAME \n")
-
-        #print(df)
-
-        print("=========== \n\n")
 
         # Obtener las columnas desde C hasta R
         columnas_valores = df.columns[2:18]  # Columnas C a R
-
-        print(" \n Columnas \n")
 
         lista_columnas = list(columnas_valores)
 
         #Lista de valores, contiene numeros del 1 al 16. Representan las categorias del EMAE.
         rango_columnas = range(1,len(columnas_valores) + 1)
-        #print("Rango DE COLUMNAS", rango_columnas)
-
-
-        print("=========== \n\n") 
-
-        
+   
         fecha_inicio = datetime(2003, 12, 1)
         num_meses = len(df) - 2  # Restar 2 para compensar las filas de encabezados
 
@@ -121,9 +103,7 @@ class cargaIndice:
         
         if row_count_after > row_count_before:
             print("Se agregaron nuevos datos")
-
             InformesEmae(host, user, password, database).enviar_mensajes()
-            #self.enviar_correo()   
         else:
             print("Se realizo una verificacion de la base de datos")
             
@@ -134,214 +114,4 @@ class cargaIndice:
         self.conn.close()
             
 
-
-    def enviar_correo(self):
-        email_emisor='departamientoactualizaciondato@gmail.com'
-        email_contraseña = 'cmxddbshnjqfehka'
-        #email_receptores =  ['benitezeliogaston@gmail.com', 'matizalazar2001@gmail.com','rigonattofranco1@gmail.com','boscojfrancisco@gmail.com','joseignaciobaibiene@gmail.com','ivanfedericorodriguez@gmail.com','agusssalinas3@gmail.com', 'rociobertonem@gmail.com','lic.leandrogarcia@gmail.com','pintosdana1@gmail.com', 'paulasalvay@gmail.com']
-        email_receptores =  ['benitezeliogaston@gmail.com', 'matizalazar2001@gmail.com']
-        asunto = 'Actualizacion de los datos del Estimador Mensual de Actividad Económico (EMAE)'
-
-
-        df_mensual,df_interanual,df_acumulado,fecha_maxima = self.variaciones_mensual_interanual_acumulada()
-
-        #Construimos la cadena de la fecha actual
-        cadena_fecha_actual = self.obtener_fecha_actual(fecha_maxima)
-    
-        cadena_inicio = f'''
-
-        <html>
-        <body>
-
-        <h3> ACTUALIZACION DE DATOS, se actualizaron los datos del Estimador Mensual de Actividad Económico(EMAE). Fecha: {cadena_fecha_actual} </h3>
-        
-        <hr>
-        '''
-
-        # Cabeza de tabla
-        cabeza_tabla_variaciones = f'''
-
-        <h3> Variaciones a nivel Nacional del Estimador Mensual de Actividad Económico (EMAE)- Argentina </h3>
-
-        
-        <table style="border-collapse: collapse; width: 100%;">
-
-        <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;"> INDICE </th>
-        <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">  VAR. MENSUAL </th>
-        <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;"> INDICE </th>
-        <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;"> VAR. INTERANUAL </th>
-        <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;"> INDICE </th>
-        <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;"> VAR. ACUMULADA</th>
-        '''
-
-
-        #Datos correspondientes a cada variacion
-        for nombre_mensual,var_mensual,nombre_interanual,var_interanual, nombre_acumulado,var_acumulada in zip(df_mensual['nombre_indices'],df_mensual['var_mensual'],df_interanual['nombre_indices'],df_interanual['var_interanual'],df_acumulado['nombre_indices'],df_acumulado['var_acumulada']):
-        
-            fila_de_nea = f'''
-                <tr>
-
-                 <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"> {nombre_mensual}</td>
-                 <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"> {var_mensual:.2f}%</td>
-
-                 <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"> {nombre_interanual}</td>
-                 <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"> {var_interanual:.2f}%</td>
-
-                 <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"> {nombre_acumulado}</td>
-                 <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;"> {var_acumulada:.2f}%</td>
-                </tr>
-                '''
-
-            cabeza_tabla_variaciones = cabeza_tabla_variaciones + fila_de_nea
-    
-
-        fin_mensaje = f'''
-        </table> 
-
-
-        <hr>
-
-        <p> Instituto Provincial de Estadistica y Ciencia de Datos de Corrientes<br>
-            Dirección: Tucumán 1164 - Corrientes Capital<br>
-            Contacto Coordinación General: 3794 284993</p>
-
-        </body>
-        </html>
-        '''
-
-
-
-        mensaje_final = cadena_inicio + cabeza_tabla_variaciones + fin_mensaje
-
-
-
-        em = EmailMessage()
-        em['From'] = email_emisor
-        em['To'] = email_receptores
-        em['Subject'] = asunto
-        em.set_content(mensaje_final, subtype = 'html')
-        
-        contexto= ssl.create_default_context()
-        
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=contexto) as smtp:
-            smtp.login(email_emisor, email_contraseña)
-            smtp.sendmail(email_emisor, email_receptores, em.as_string())
-
-
-
-    def variaciones_mensual_interanual_acumulada(self):
-        
-        #Buscamos los datos de la tabla emae, y lo transformamos a un dataframe
-        nombre_tabla = 'emae'
-        query_select = f'SELECT * from {nombre_tabla}' 
-        df_bdd = pd.read_sql(query_select,self.conn)
-        df_bdd['Fecha'] = pd.to_datetime(df_bdd['Fecha'])#--> Cambiamos formato de la fecha para su manipulacion
-        
-        #Buscamos los datos de las categorias del emae, para lograr un for con cada indice, y para organizar la tabla por |INDICE|VALOR
-        nombre_tabla = 'emae_categoria'
-        query_select = f'SELECT * from {nombre_tabla}' 
-        df_categorias = pd.read_sql(query_select,self.conn)
-
-
-        #OBTENCION DEL GRUPO DE LA FECHA MAXIMA 
-        fecha_maxima = max(df_bdd['Fecha'])
-        df_bdd_ultima_fecha = df_bdd[df_bdd['Fecha'] == fecha_maxima]
-
-
-        #LISTAS que acumulan consecutivamente los indices y las variaciones
-        lista_indices = []
-        lista_var_mensual = []
-        lista_var_interanual = []
-        lista_var_acumulada = []
-
-        for indice,descripcion_categoria in zip(df_categorias['id_categoria'],df_categorias['categoria_descripcion']):
-
-
-            #Obtenemos el valor actual y el valor del mes anterior de la misma categoria --> SE USA EL MISMO VALOR PARA CADA VARIACION
-            valor_actual = df_bdd_ultima_fecha['Valor'][df_bdd_ultima_fecha['Sector_Productivo'] == indice].values[0]
-
-            # === CALCULO VARIACION MENSUAL
-
-            valor_mes_anterior = df_bdd['Valor'][ (df_bdd['Fecha'].dt.year == fecha_maxima.year)
-                                                & (df_bdd['Fecha'].dt.month == fecha_maxima.month - 1)
-                                                & (df_bdd['Sector_Productivo'] == indice)].values[0]
-
-            #Calculo final
-            var_mensual = ((valor_actual / valor_mes_anterior) - 1) * 100
-
-            # === CALCULO VARIACION INTERANUAL
-            
-            valor_año_anterior = df_bdd['Valor'][ (df_bdd['Fecha'].dt.year == fecha_maxima.year - 1)
-                                                & (df_bdd['Fecha'].dt.month == fecha_maxima.month)
-                                                & (df_bdd['Sector_Productivo'] == indice)].values[0]
-
-            #Calculo final
-            var_intearnual = ((valor_actual / valor_año_anterior) - 1) * 100
-
-            # === CALCULO VARIACION ACUMULADA    
-            valor_diciembre_año_anterior = df_bdd['Valor'][ (df_bdd['Fecha'].dt.year == fecha_maxima.year - 1)
-                                                & (df_bdd['Fecha'].dt.month == 12)
-                                                & (df_bdd['Sector_Productivo'] == indice)].values[0]
-
-            #Calculo final
-            var_acumulada = ((valor_actual / valor_diciembre_año_anterior) - 1) * 100
-
-
-
-            #Agregamos cada valor a su correspondiente lista
-            lista_indices.append(descripcion_categoria)
-            lista_var_mensual.append(var_mensual)
-            lista_var_interanual.append(var_intearnual)
-            lista_var_acumulada.append(var_acumulada)
-
-
-        
-
-        #Creacion de DATAFRAME
-        data = {
-                
-                'nombre_indices':lista_indices,
-                'var_mensual': lista_var_mensual,
-                'var_interanual':lista_var_interanual,
-                'var_acumulada':lista_var_acumulada
-        }
-
-        df = pd.DataFrame(data)
-
-        
-        #Ordenacion por var mensual | intearanual| acumulado
-        df_mensual = df.sort_values(by='var_mensual',ascending=[False])
-        df_interanual = df.sort_values(by='var_interanual',ascending=[False])
-        df_acumulado = df.sort_values(by='var_acumulada',ascending=[False])
-        
-        return df_mensual,df_interanual,df_acumulado, fecha_maxima
-
-
-    
-    def obtener_fecha_actual(self,fecha_ultimo_registro):
-        
-
-        # Obtener el nombre del mes actual en inglés
-        nombre_mes_ingles = calendar.month_name[fecha_ultimo_registro.month]
-
-        # Diccionario de traducción
-        traducciones_meses = {
-            'January': 'Enero',
-            'February': 'Febrero',
-            'March': 'Marzo',
-            'April': 'Abril',
-            'May': 'Mayo',
-            'June': 'Junio',
-            'July': 'Julio',
-            'August': 'Agosto',
-            'September': 'Septiembre',
-            'October': 'Octubre',
-            'November': 'Noviembre',
-            'December': 'Diciembre'
-        }
-
-        # Obtener la traducción
-        nombre_mes_espanol = traducciones_meses.get(nombre_mes_ingles, nombre_mes_ingles)
-
-        return str(fecha_ultimo_registro.day) + f" de {nombre_mes_espanol} del {fecha_ultimo_registro.year}"
 
