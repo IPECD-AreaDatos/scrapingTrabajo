9802bbfc41cbe3469b7a80484d114aab7917dddc
gastonbenitez2001 <gastongrillo2001@gmail.com>
2024-04-09 09:20:49 -0300

    Salarios

--- scrap_SalarioSP-Total/main.py
+++ scrap_SalarioSP-Total/main.py
@@ -1,5 +1,5 @@
 from homePage import HomePage
-from loadCSVData_SP import loadCSVData_SP
+from loadCSVData_SP import Gestion_bdd
 from loadCSVData_Total import loadCSVData_Total
 import sys
 import os
@@ -15,19 +15,28 @@ sys.path.append(credenciales_dir)
 from credenciales_bdd import Credenciales
 
 
-credenciales = Credenciales('ipecd_economico')
-
+credenciales = Credenciales('datalake_economico')
+credenciales_local = Credenciales('ipecd_economico')
 
 if __name__ == '__main__':
     #home_page = HomePage()
     #home_page.descargar_archivo()
 
+    #Obtencion de los datos con el formato correcto 
+    df_salario_sp = transform.datos_sp() #--> Datos del PRIVADO
+    df_datos_totales = transform.datos_totales() #--> Datos TOTALES
     
-    df_salario_sp = transform.datos_sp()
-    df_datos_totales = transform.datos_totales()
+    #Creamos instancia para cargar datos en el DATALAKE
+    instancia = Gestion_bdd(host=credenciales.host, user=credenciales.user, password=credenciales.password, database=credenciales.database)
+    instancia_local = Gestion_bdd(host=credenciales_local.host, user=credenciales_local.user, password=credenciales_local.password, database=credenciales_local.database)
 
-    sys.exit()
+    #Carga de DATALAKE de datos de Salario Privado
+    instancia.loadInDataBase(df_salario_sp,'dp_salarios_sector_privado')
+    instancia.loadInDataBase(df_datos_totales,'dp_salarios_total')
 
-    loadCSVData_SP().loadInDataBase(credenciales.host, credenciales.user, credenciales.password,credenciales.database)
+    #Carga de IPECD ECONOMICO - Para tablero de Economico.
+    instancia_local.loadInDataBase(df_salario_sp,'dp_salarios_sector_privado')
+    instancia_local.loadInDataBase(df_datos_totales,'dp_salarios_total')
 
-    loadCSVData_Total().loadInDataBase(credenciales.host, credenciales.user, credenciales.password, credenciales.database)
+
+
