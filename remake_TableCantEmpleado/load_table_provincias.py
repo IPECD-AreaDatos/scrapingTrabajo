5a96f4bb41e5b8a4972f956bc842c7fc4e36a373
Zalmatt <matizalazar2001@gmail.com>
2024-04-24 17:52:00 -0300

    Cada mil habitantes

--- remake_TableCantEmpleado/load_table_provincias.py
+++ remake_TableCantEmpleado/load_table_provincias.py
@@ -19,7 +19,7 @@ class load_table_provincia:
         return self
     
     def read_database(self):
-        from datetime import date
+        from datetime import date, timedelta
         id_provincia = 2
         start_date = date(2010, 1, 1)
         end_date = date(2025, 1, 1)
@@ -30,17 +30,43 @@ class load_table_provincia:
             current_date = current_date.replace(year=current_date.year + 1)
             date_list.append(current_date)
         results = {}
+        
+        # Consulta para obtener las fechas disponibles en sipa_registro para la provincia
+        query_fechas_sipa = """
+        SELECT fecha FROM ipecd_economico.sipa_registro
+        WHERE id_provincia = %s
+        ORDER BY fecha DESC;
+        """
+        self.cursor.execute(query_fechas_sipa, (id_provincia,))
+        fechas_sipa = [row[0] for row in self.cursor.fetchall()]
+        
         for date in date_list:
-            query = """
+            # Encuentra la fecha más cercana al inicio del año en sipa_registro
+            sipa_date = max(fecha for fecha in fechas_sipa if fecha < date)
+            
+            # Consulta para obtener la población total
+            query_poblacion = """
             SELECT SUM(Poblacion) FROM ipecd_economico.censo_provincia
             WHERE ID_Provincia = %s AND fecha = %s;
             """
-            self.cursor.execute(query, (id_provincia, date.strftime('%Y-%m-%d')))
-            result = self.cursor.fetchone()  # fetchone() because SUM returns a single row with the sum value
+            self.cursor.execute(query_poblacion, (id_provincia, date.strftime('%Y-%m-%d')))
+            result_poblacion = self.cursor.fetchone()
+            total_poblacion = result_poblacion[0] if result_poblacion[0] is not None else 0
 
-            # Fetchone returns a tuple, and since we're summing one column, we access the first element.
-            total_sum = result[0] if result[0] is not None else 0
-            results[date.strftime('%Y-%m-%d')] = total_sum
+            # Consulta para obtener el total de empleo sin estacionalidad de la fecha correspondiente en sipa_registro
+            query_empleo = """
+            SELECT SUM(cantidad_sin_estacionalidad) FROM ipecd_economico.sipa_registro
+            WHERE id_provincia = %s AND fecha = %s;
+            """
+            self.cursor.execute(query_empleo, (id_provincia, sipa_date))
+            result_empleo = self.cursor.fetchone()
+            total_empleo = result_empleo[0] if result_empleo[0] is not None else 0
+
+            if total_poblacion > 0:  # Evitar división por cero
+                empleo_por_mil = ((total_empleo *1000) / total_poblacion) * 1000
+            else:
+                empleo_por_mil = 0
+            results[date.strftime('%Y')] = empleo_por_mil
 
         return results
-                
+                    
