447cb7737d48b0c6df7ae3774b5ce8c961c3b4e3
Zallmatt <matizalazar2001@gmail.com>
2024-06-03 12:42:52 -0300

    IPC_Online

--- remake_TableCantEmpleado/main.py
+++ remake_TableCantEmpleado/main.py
@@ -5,23 +5,15 @@ from load_sipa_valores import load_sipa_valores
 from load_database import load_database
 import pandas as pd
 
-# Obtener la ruta al directorio actual del script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 credenciales_dir = os.path.join(script_dir, "..", "Credenciales_folder")
-# Agregar la ruta al sys.path
 sys.path.append(credenciales_dir)
-# Importar las credenciales
-
-# Documentación:
-# - Importaciones necesarias para el script.
-# - Obtención de las credenciales necesarias para la conexión a bases de datos.
-# - Bases con el mismo nombre / local_... para las del servidor local
 from credenciales_bdd import Credenciales
-# Después puedes crear una instancia de Credenciales
 credenciales_dwh_economico = Credenciales("dwh_economico")
 credenciales_ipecd_economico = Credenciales("ipecd_economico")
 credenciales_datalake_economico = Credenciales("datalake_economico")
-# Instancia encargada
+
+#Fecha de recoleccion de dato
 star_date = (2010)
 end_year = (2024)
 
