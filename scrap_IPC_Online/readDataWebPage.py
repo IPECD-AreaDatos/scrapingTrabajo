8aea6ca4a481dc7b3f88ba226baa1e1a5c32351e
Zallmatt <matizalazar2001@gmail.com>
2024-06-12 11:19:13 -0300

    ipc online

--- scrap_IPC_Online/readDataWebPage.py
+++ scrap_IPC_Online/readDataWebPage.py
@@ -1,7 +1,8 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import re
+from datetime import datetime
 
 class readDataWebPage:
     def __init__(self):
@@ -21,6 +22,12 @@ class readDataWebPage:
         self.driver = webdriver.Chrome(options=options)
         self.original_window = self.driver.current_window_handle
 
+    def convertir_numerico(self, dato):
+        dato = dato.replace({'%': '', ',': '.'}, regex=True)
+        dato = float(dato)
+        return dato
+
+
     def _extract_recent_data(self):
         try:
             # Esperar a que la página cargue completamente
@@ -32,20 +39,57 @@ class readDataWebPage:
             # Extraer el texto del elemento y guardarlo en la variable 'data_variacion_mensual'
             data_variacion_mensual = variacion_mensual_element.text
 
+            data_variacion_mensual = re.sub(r'%|,', '', data_variacion_mensual)
+            data_variacion_mensual = float(data_variacion_mensual)
+            data_variacion_mensual /= 100
+
+
             # Buscar el elemento con el dato usando XPath
             variaciones_element = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div/main/article[1]/div[1]/p[1]')
 
             # Extraer el texto del elemento y guardarlo en la variable 'data_variaciones'
             data_variaciones = variaciones_element.text
+            numeros = re.findall(r'\d+,\d+', data_variaciones)
+
+            # Procesar los números encontrados
+            numeros_procesados = [float(numero.replace(',', '.')) for numero in numeros]
 
             # Buscar el elemento con el dato usando XPath
             fecha = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div/main/article/header/h1')
             # Extraer el texto del elemento y guardarlo en la variable 'data_variaciones'
             data_fecha = fecha.text
+            match = re.search(r'([A-Za-z]+) (\d{4})', data_fecha)
+
+            if match:
+                # Extraer el mes y el año
+                nombre_mes = match.group(1).lower()  # Convertir el nombre del mes a minúsculas
+                year = int(match.group(2))
+
+                # Mapear el nombre del mes a su representación numérica
+                meses = {
+                    "enero": 1, "febrero": 2, "marzo": 3, "abril": 4, "mayo": 5, "junio": 6,
+                    "julio": 7, "agosto": 8, "septiembre": 9, "octubre": 10, "noviembre": 11, "diciembre": 12
+                }
+
+                if nombre_mes in meses:
+                    # Construir la fecha con día fijo (1)
+                    fecha_obj = datetime(year, meses[nombre_mes], 1).date()
+
+                    print("Fecha extraída:", fecha_obj)
+                else:
+                    print("Nombre del mes no válido:", nombre_mes)
+            else:
+                print("No se encontró la fecha en el texto extraído.")
+
             # Imprimir el dato por pantalla
-            print(f"Dato fecha: {data_fecha}")
+            print(f"Dato fecha: {fecha_obj}")
+            print(type(fecha_obj))
             print(f"Dato variacion mensual: {data_variacion_mensual}")
-            print(f"Dato variaciones: {data_variaciones}")
+            print(type(data_variacion_mensual))
+            print(f"Cobertura IPCNu: {numeros_procesados[0]}")
+            print(f"Variación acumulada interanual: {numeros_procesados[1]}")
+            print(f"Variación acumulada en 2024: {numeros_procesados[2]}")
+
 
             # Cerrar el navegador después de encontrar los dos datos
             self.driver.quit()
