36bb59e73a062221fe442302c3a73647661b1e15
Zallmatt <matizalazar2001@gmail.com>
2024-06-12 12:03:16 -0300

    ipc online carga

--- scrap_IPC_Online/main.py
+++ scrap_IPC_Online/main.py
@@ -1,6 +1,29 @@
 from readDataWebPage import readDataWebPage
+from loadDataBase import loadDataBase
+import os
+import sys
 
 
+# Obtener la ruta al directorio actual del script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+credenciales_dir = os.path.join(script_dir, "..", "Credenciales_folder")
+# Agregar la ruta al sys.path
+sys.path.append(credenciales_dir)
+# Importar las credenciales
+
+# Documentación:
+# - Importaciones necesarias para el script.
+# - Obtención de las credenciales necesarias para la conexión a bases de datos.
+# - Bases con el mismo nombre / local_... para las del servidor local
+from credenciales_bdd import Credenciales
+# Después puedes crear una instancia de Credenciales
+credenciales_datalake_economico = Credenciales("datalake_economico")
 
 if __name__ == '__main__':
-    readDataWebPage().extract_data()
+    df = readDataWebPage().extract_data()
+    print(df)
+
+    credenciales = loadDataBase(credenciales_datalake_economico.host, credenciales_datalake_economico.user, credenciales_datalake_economico.password, credenciales_datalake_economico.database).conectar_bdd()
+
+    credenciales.cargaBaseDatos(df)
+
