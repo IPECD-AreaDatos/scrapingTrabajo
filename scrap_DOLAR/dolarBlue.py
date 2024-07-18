7d5e01b55de9bd2776f2fb24ed29168ff89efd62
Zallmatt <matizalazar2001@gmail.com>
2024-07-18 11:56:40 -0300

    Dolar actualizado

--- scrap_DOLAR/dolarBlue.py
+++ scrap_DOLAR/dolarBlue.py
@@ -1,31 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import urllib3
 from datetime import datetime, timedelta
 from sqlalchemy import create_engine, text
 import mysql.connector
 from io import StringIO
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
+import time
 
 class dolarBlue:
-    def __init__(self, host, user, password, database, fecha_inicio, fecha_fin):
+    def __init__(self, host, user, password, database):
         self.host = host
         self.user = user
         self.password = password
         self.database = database
         self.url_blue = 'https://www.ambito.com/contenidos/dolar-informal-historico.html'
-        self.fecha_inicio = datetime.strptime(fecha_inicio, "%d/%m/%Y")
-        self.fecha_fin = datetime.strptime(fecha_fin, "%d/%m/%Y")
         self.setup_driver()
+        self.fecha_actual = datetime.now().strftime("%d/%m/%Y")
 
     def setup_driver(self):
         chrome_options = Options()
         chrome_options.add_argument("--disable-notifications")
         chrome_options.add_argument("--disable-popup-blocking")
         chrome_options.add_argument("--start-maximized")
+        #chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu") 
         self.driver = webdriver.Chrome(options=chrome_options) 
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -35,99 +37,133 @@ class dolarBlue:
         try:
             popup = wait.until(EC.element_to_be_clickable((By.ID, "onesignal-slidedown-cancel-button")))
             popup.click()
-        except Exception:
-            print("No se encontró el popup de notificaciones o no se pudo cerrar.")
+        except Exception as e:
+            print(f"No se encontró el popup de notificaciones o no se pudo cerrar: {e}")
         try:
             ad_close_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'close-button')]")))
             ad_close_button.click()
-        except Exception:
-            print("No se encontró la publicidad emergente o no se pudo cerrar.")
+        except Exception as e:
+            print(f"No se encontró la publicidad emergente o no se pudo cerrar: {e}")
 
     def wait_for_element(self, by, identifier):
         wait = WebDriverWait(self.driver, 30)
         return wait.until(EC.visibility_of_element_located((by, identifier)))
 
     def tomaDolarBlue(self):
-        fecha_actual = self.fecha_inicio
-
-        while fecha_actual <= self.fecha_fin:
-            fecha_siguiente = fecha_actual + timedelta(days=1)
+        try:
+            self.driver.get(self.url_blue)
+            print("Página cargada con éxito")
+            
+            # Cerrar popups de notificaciones y publicidad
             try:
-                self.driver.get(self.url_blue)
-
-                # Cerrar popups de notificaciones y publicidad
                 self.close_popups()
-
-                # Encontrar y rellenar las fechas
+            except Exception as e:
+                print(f"No se encontró el popup de notificaciones o no se pudo cerrar: {e}")
+            
+            # Encontrar los campos de fecha y el botón de búsqueda
+            try:
                 fecha_desde = self.wait_for_element(By.CLASS_NAME, "general-historical__datepicker.datepicker.desde.form-control")
                 fecha_hasta = self.wait_for_element(By.CLASS_NAME, "general-historical__datepicker.datepicker.hasta.form-control")
-
-                fecha_desde.clear()
-                fecha_desde.send_keys(fecha_actual.strftime("%d/%m/%Y"))
-                fecha_hasta.clear()
-                fecha_hasta.send_keys(fecha_siguiente.strftime("%d/%m/%Y"))
-
                 boton = self.wait_for_element(By.CLASS_NAME, "general-historical__button")
-                
-                # Intentar hacer clic en el botón, manejando posibles elementos que intercepten el clic
+                print("Campos de fecha y botón encontrados")
+            except Exception as e:
+                print(f"No se encontraron los campos de fecha o el botón: {e}")
+                return
+            
+            # Crear un DataFrame vacío para almacenar los datos
+            df_total = pd.DataFrame(columns=['fecha', 'compra', 'venta'])
+
+            # Usar solo la fecha actual
+            dia_actual = self.fecha_actual
+            fecha_desde.clear()
+            fecha_desde.send_keys(dia_actual)
+            fecha_hasta.clear()
+            fecha_hasta.send_keys(dia_actual)
+
+            try:
+                boton.click()
+                print(f"Botón clicado para la fecha: {dia_actual}")
+            except Exception as e:
+                print(f"Elemento click interceptado para la fecha {dia_actual}, intentando desplazarse y hacer clic nuevamente: {e}")
                 try:
-                    boton.click()
-                except Exception:
-                    print("Elemento click interceptado, intentando desplazarse y hacer clic nuevamente...")
                     self.driver.execute_script("arguments[0].scrollIntoView(true);", boton)
                     WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "general-historical__button")))
                     boton.click()
+                    print(f"Botón clicado después de desplazar para la fecha: {dia_actual}")
+                except Exception as e:
+                    print(f"No se pudo hacer clic en el botón para la fecha {dia_actual}: {e}")
+                    return
 
-                # Esperar explícitamente hasta que la tabla esté presente y se haya actualizado
-                table = WebDriverWait(self.driver, 10).until(
+            # Esperar a que la tabla se cargue
+            try:
+                time.sleep(3)
+                table = WebDriverWait(self.driver, 20).until(
                     EC.presence_of_element_located((By.CLASS_NAME, 'general-historical__table'))
                 )
-
-                # Esperar hasta que la tabla contenga al menos un dato en el tbody
-                WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, '.general-historical__tbody tbody tr'))
-                )
-
-                table_html = table.get_attribute('outerHTML').replace(',', '.')
-
-                # Usar StringIO para manejar FutureWarning
-                df = pd.read_html(StringIO(table_html))[0]
-                df.columns = ['fecha', 'compra', 'venta']  # Asegurar que las columnas tienen los nombres correctos
-                df['fecha'] = pd.to_datetime(df['fecha'], format='%d/%m/%Y').dt.strftime('%Y-%m-%d')
-
-                # Insertar los datos en la base de datos
-                self.update_database(df)
-                print(f"Datos obtenidos y actualizados para la fecha: {fecha_actual.strftime('%d/%m/%Y')}")
-
+                print(f"Tabla encontrada para la fecha: {dia_actual}")
             except Exception as e:
-                print(f"Error al obtener datos para la fecha: {fecha_actual.strftime('%d/%m/%Y')} - {e}")
+                print(f"No se encontró la tabla para la fecha {dia_actual}: {e}")
+                return
 
-            fecha_actual = fecha_siguiente
+            # Verificar el contenido de la tabla antes de leerla
+            try:
+                table_html = table.get_attribute('outerHTML').replace(',', '.')
+                df = pd.read_html(StringIO(table_html))[0]
+                if not df.empty:
+                    if len(df.columns) == 4:
+                        df = df.iloc[:, 1:4]
+                    df['fecha'] = datetime.strptime(dia_actual, "%d/%m/%Y").strftime('%Y-%m-%d')  # Convertir la fecha al formato YYYY-MM-DD
+                    df.columns = ['fecha', 'compra', 'venta']
+                    print(df)
+                    df_total = pd.concat([df_total, df], ignore_index=True)
+                else:
+                    print(f"No hay datos disponibles para la fecha {dia_actual}")
+                    return
+            except Exception as e:
+                print(f"Error al leer la tabla HTML para la fecha {dia_actual}: {e}")
+                return
+
+            # Verificar la última fecha en la base de datos
+            if self.check_last_date(df_total):
+                # Actualizar la base de datos con el DataFrame total
+                self.update_database(df_total)
+            else:
+                print("La fecha actual ya está presente en la base de datos o no es posterior a la última fecha")
+
+        except Exception as e:
+            print(f"Error al obtener datos del dólar blue: {e}")
+        finally:
+            self.driver.quit()
+
+    def check_last_date(self, df):
+        try:
+            engine = create_engine(f'mysql+mysqlconnector://{self.user}:{self.password}@{self.host}/{self.database}')
+            with engine.connect() as connection:
+                last_date_query = text("SELECT MAX(fecha) FROM dolar_blue")
+                result = connection.execute(last_date_query).scalar()
+                if result:
+                    last_date = datetime.strptime(result, '%Y-%m-%d')
+                    current_date = datetime.strptime(df['fecha'].iloc[0], '%Y-%m-%d')
+                    return current_date > last_date
+                return True
+        except mysql.connector.Error as err:
+            print(f"Error MySQL: {err}")
+            return False
+        finally:
+            engine.dispose()
 
     def update_database(self, df):
         try:
             engine = create_engine(f'mysql+mysqlconnector://{self.user}:{self.password}@{self.host}/{self.database}')
-
             table_name = 'dolar_blue'
 
-            with engine.connect() as connection:
-                for index, row in df.iterrows():
-                    query = text(f"""
-                    INSERT INTO {table_name} (fecha, compra, venta)
-                    VALUES (:fecha, :compra, :venta)
-                    ON DUPLICATE KEY UPDATE
-                        compra = VALUES(compra),
-                        venta = VALUES(venta)
-                    """)
-                    connection.execute(query, {
-                        'fecha': row['fecha'],
-                        'compra': row['compra'],
-                        'venta': row['venta']
-                    })
-            print("Datos actualizados en la base de datos")
+            # Insertar los nuevos datos sin truncar la tabla
+            df.to_sql(table_name, con=engine, if_exists='append', index=False)
+            print("Se actualizaron los datos en la base de datos")
 
         except mysql.connector.Error as err:
             print(f"Error MySQL: {err}")
 
         finally:
             engine.dispose()
+
--- scrap_DOLAR/main.py
+++ scrap_DOLAR/main.py
@@ -3,12 +3,12 @@ En este script lo que vamos a hacer es rescatar los valores de distintos tipos d
 
 - Dolar oficial: de banco nacion
 - Dolar MEP, BLUE y CCL: sacado de https://www.ambito.com/ --> Sitio web dedicado al ambito bursatil
+
 """
 
 from dolarOficial import dolarOficial
 import os
 import sys
-import pandas as pd
 from datetime import datetime
 
 # Obtener la ruta al directorio actual del script
@@ -20,31 +20,15 @@ sys.path.append(credenciales_dir)
 from credenciales_bdd import Credenciales
 # Después puedes crear una instancia de Credenciales
 credenciales = Credenciales('datalake_economico')
-
 from dolarBlue import dolarBlue
+#from dolarBlueHistorico import dolarBlueHistorico
+#dolarBlueHistorico(credenciales.user, credenciales.password, credenciales.host, credenciales.database, 'dolar_blue').load_xlsx_to_mysql()
 from dolarMEP import dolarMEP
 from dolarCCL import dolarCCL
 
-# Definir el rango de fechas
-fecha_inicio = "01/01/2003"
-fecha_fin = "31/12/2003"
-
-# Convertir a objetos datetime
-start_date = datetime.strptime(fecha_inicio, "%d/%m/%Y")
-end_date = datetime.strptime(fecha_fin, "%d/%m/%Y")
-
-if __name__ == '__main__':
-    # dolarOficial().descargaArchivo()
-    # dolarOficial().lecturaDolarOficial(credenciales.host, credenciales.user, credenciales.password, credenciales.database)
-    
-    try:
-        # Llamar a la función tomaDolarBlue para el rango de fechas
-        print(f"Ejecutando tomaDolarBlue para el rango de fechas: {start_date.strftime('%d/%m/%Y')} - {end_date.strftime('%d/%m/%Y')}")
-        db_scraper = dolarBlue(credenciales.host, credenciales.user, credenciales.password, credenciales.database, fecha_inicio=fecha_inicio, fecha_fin=fecha_fin)
-        db_scraper.tomaDolarBlue()
-        print(f"Datos del dólar blue obtenidos para el rango de fechas: {start_date.strftime('%d/%m/%Y')} - {end_date.strftime('%d/%m/%Y')}.")
-    except Exception as e:
-        print(f"Error al obtener datos del dólar blue para el rango de fechas: {start_date.strftime('%d/%m/%Y')} - {end_date.strftime('%d/%m/%Y')}: {e}")
-    
-    # dolarMEP().tomaDolarMEP(credenciales.host, credenciales.user, credenciales.password, credenciales.database)
-    # dolarCCL().tomaDolarCCL(credenciales.host, credenciales.user, credenciales.password, credenciales.database)
+if __name__ == '__main__': 
+    dolarOficial().descargaArchivo()
+    dolarOficial().lecturaDolarOficial(credenciales.host, credenciales.user, credenciales.password, credenciales.database)
+    dolarBlue(credenciales.host, credenciales.user, credenciales.password, credenciales.database).tomaDolarBlue()
+    #dolarMEP().tomaDolarMEP(credenciales.host, credenciales.user, credenciales.password, credenciales.database)
+    #dolarCCL().tomaDolarCCL(credenciales.host, credenciales.user, credenciales.password, credenciales.database)
