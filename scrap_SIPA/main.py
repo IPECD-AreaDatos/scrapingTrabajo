ca03d300366b56e7cddb97b00340d48f9c59c505
gastonbenitez2001 <gastongrillo2001@gmail.com>
2024-08-23 11:16:14 -0300

    borramos key de semaforo y arreglmos main de sipa

--- scrap_SIPA/main.py
+++ scrap_SIPA/main.py
@@ -60,30 +60,3 @@ if __name__ == '__main__':
         instancia_correo.connect_db()
         instancia_correo.send_mail()
 
-
-    #=== ZONA DE PRUEBA
-    from email.message import EmailMessage
-    from ssl import create_default_context
-    from smtplib import SMTP_SSL
-    from selenium import webdriver
-
-
-    #Declaramos email desde el que se envia, la contraseña de la api, y los correos receptores.
-    email_emisor='departamientoactualizaciondato@gmail.com'
-    email_contrasenia = 'cmxddbshnjqfehka'
-    email_receptores =  ['benitezeliogaston@gmail.com']
-
-    #==== Zona de envio de correo
-    em = EmailMessage()
-    em['From'] = email_emisor
-    em['To'] = email_receptores
-    em['Subject'] = "CORREO DE PRUEBA - SIPA"
-    em.set_content("Prueba de funcionamiento de SIPA.")
-
-    contexto= create_default_context()
-
-    with SMTP_SSL('smtp.gmail.com', 465, context=contexto) as smtp:
-        smtp.login(email_emisor, email_contrasenia)
-        smtp.sendmail(email_emisor, email_receptores, em.as_string())
-
-
