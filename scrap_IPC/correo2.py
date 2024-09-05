3dc199f3255aaa03eb3866f9a2a14381588dd9a8
manumarder <manumarder@gmail.com>
2024-09-05 17:05:45 -0300

    correo 2 ipc

--- scrap_IPC/correo2.py
+++ scrap_IPC/correo2.py
@@ -23,19 +23,29 @@ class InformeIPC:
         self.database = database
 
         #Creacion de conexion a la bdd
-        self.engine = create_engine('mysql+pymysql://root:tu_contraseña@localhost:3306/mi_base_de_datos')
+        self.engine = create_engine(f'mysql+pymysql://{self.user}:{self.password}@{self.host}:3306/{self.database}')
 
     # Objetivo: Juntar todos los elementos del correo y darle forma para enviarlo.
     def enviar_correo(self, fecha_maxima, var_mensual, var_acumulada, var_interanual, df_var_nea, df_var_region ):
 
         #Pasamos la ultima fecha un formato mas legible
-        cadena_fecha_actual = self.obtener_fecha_actual(fecha_maxima)
+        cadena_fecha_actual = self.obtener_fecha_actual(fecha_maxima)        
 
         #Asunto del correo
         asunto = f'Actualizacion de datos IPC - {cadena_fecha_actual}'
 
         cadena_inicio = f'''
         <html>
+        <head>
+            <style>
+                @media (max-width: 600px) {{
+                    .img-responsive {{
+                        width: 100%;
+                        height: auto;
+                    }}
+                }}
+            </style>
+        </head>
             <body style="color-scheme: light;">
                 <div class="container" style= "color-scheme: light;background-image: url('cid:fondo'); background-repeat: no-repeat; background-position: center center; background-size: cover;">
 
@@ -50,9 +60,72 @@ class InformeIPC:
                         <h4 class="variaciones-imp" style="font-size: 17px;font-weight: 200; color: white; -webkit-text-fill-color: white !important;">VARIACIÓN ACUMULADA: <strong>{var_acumulada:.2f}%</strong></h4>
                     </div>
                 </div>
+                <!-- Box con imagen a la izquierda y tabla a la derecha -->
+                <div style="display: flex; align-items: center; justify-content: center; margin: 20px 0; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+                    <!-- Imagen -->
+                    <div style="flex: 1; padding: 20px;">
+                        <img src="cid:mapa_regiones" alt="Imagen Box" class="img-responsive" style="width: 100%; height: auto; border-radius: 8px;">
+                    </div>
+                    <!-- Tabla -->
+                    <div style="flex: 2; padding: 20px;">
+                        <table style="border-collapse: separate; border-spacing: 0; width: 100%; background-color: #f9f9f9; border-radius: 8px; overflow: hidden; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+                            <thead>
+                                <tr style="background-color: #465c49; color: #fff;">
+                                    <th style="padding: 10px; text-align: left; font-size: 16px;">Region</th>
+                                    <th style="padding: 10px; text-align: left; font-size: 16px;">Variación Mensual</th>
+                                    <th style="padding: 10px; text-align: left; font-size: 16px;">Variación Interanual</th>
+                                    <th style="padding: 10px; text-align: left; font-size: 16px;">Variación Acumulada</th>
+                                </tr>
+                            </thead>
+                            <tbody>
         '''
+        
+        # Añadir filas a la tabla para df_var_region
+        for _, row in df_var_region.iterrows():
+            cadena_inicio += f'''
+                <tr style="background-color: #fff; border-bottom: 1px solid #ddd;">
+                    <td style="padding: 10px; font-size: 14px;">{row['id_region']}</td>
+                    <td style="padding: 10px; font-size: 14px; color: #333;">{row['var_mensual']}</td>
+                    <td style="padding: 10px; font-size: 14px; color: #333;">{row['var_interanual']}</td>
+                    <td style="padding: 10px; font-size: 14px; color: #333;">{row['var_acumulada']}</td>
+                </tr>
+            '''
+        
+        cadena_inicio += f'''
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                
+                <!-- Tabla de variaciones NEA -->
+                <div style="text-align: center; margin: 20px 0;">
+                    <table style="border-collapse: separate; border-spacing: 0; margin: 0 auto; width: 80%; max-width: 600px; background-color: #f9f9f9; border-radius: 8px; overflow: hidden; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+                        <thead>
+                            <tr style="background-color: #465c49; color: #fff;">
+                                <th style="padding: 10px; text-align: left; font-size: 16px;">Categoría</th>
+                                <th style="padding: 10px; text-align: left; font-size: 16px;">Variación Mensual</th>
+                                <th style="padding: 10px; text-align: left; font-size: 16px;">Variación Interanual</th>
+                                <th style="padding: 10px; text-align: left; font-size: 16px;">Variación Acumulada</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+        '''
+        
+        # Añadir filas a la tabla para df_var_nea
+        for _, row in df_var_nea.iterrows():
+            cadena_inicio += f'''
+                <tr style="background-color: #fff; border-bottom: 1px solid #ddd;">
+                    <td style="padding: 10px; font-size: 14px;">{row['id_categoria']}</td>
+                    <td style="padding: 10px; font-size: 14px; color: #333;">{row['var_mensual']}</td>
+                    <td style="padding: 10px; font-size: 14px; color: #333;">{row['var_interanual']}</td>
+                    <td style="padding: 10px; font-size: 14px; color: #333;">{row['var_acumulada']}</td>
+                </tr>
+            '''
 
         fin_mensaje = f'''
+                        </tbody>
+                    </table>
+                </div>
                 <br>
                 <div class="footer" style="font-size: 15px; color: #888; text-align: center" >
                     <img src="cid:ipecd" alt="logo" style="margin-right: 20px; max-width: 250px; height: auto; pointer-events: none; user-select: none;" >
@@ -70,7 +143,7 @@ class InformeIPC:
         email_emisor='departamientoactualizaciondato@gmail.com'
         email_contraseña = 'cmxddbshnjqfehka'
         #email_receptores =  ['benitezeliogaston@gmail.com', 'matizalazar2001@gmail.com','manumarder@gmail.com','rigonattofranco1@gmail.com','boscojfrancisco@gmail.com','joseignaciobaibiene@gmail.com','ivanfedericorodriguez@gmail.com','agusssalinas3@gmail.com', 'rociobertonem@gmail.com','lic.leandrogarcia@gmail.com','pintosdana1@gmail.com', 'paulasalvay@gmail.com']
-        email_receptores =  ['matizalazar2001@gmail.com','manumarder@gmail.com']
+        email_receptores =  ['benitezeliogaston@gmail.com','matizalazar2001@gmail.com','manumarder@gmail.com']
         email_receptores_str = ', '.join(email_receptores)
 
         em = MIMEMultipart()
@@ -88,7 +161,8 @@ class InformeIPC:
         image_files = {
             "ipecd": "logo_ipecd.png", 
             "fondo": "fondo_correo.png",
-            "fondo_cuadros": "fondo_cuadros.png"
+            "fondo_cuadros": "fondo_cuadros.png",
+            "mapa_regiones": "mapa_regiones.webp"
         }
 
         # Construir las rutas completas y crear un diccionario para las rutas de las imágenes
@@ -109,6 +183,10 @@ class InformeIPC:
 
     # Objetivo: Convertir a formato string una fecha
     def obtener_fecha_actual(self,fecha_ultimo_registro):
+
+        # Verificar si la fecha es una cadena, y convertirla a objeto datetime si es necesario
+        if isinstance(fecha_ultimo_registro, str):
+            fecha_ultimo_registro = datetime.strptime(fecha_ultimo_registro, '%Y-%m-%d %H:%M:%S')
 
         # Obtener el nombre del mes actual en inglés
         nombre_mes_ingles = calendar.month_name[fecha_ultimo_registro.month]
@@ -161,9 +239,27 @@ class InformeIPC:
     # Objetivo: Leer de la base las variaicones por region en la ultima fecha para tranformarlo en un df
     def variaciones_region(self, fecha):
         nombre_tabla = 'ipc_valores'
-        query_consulta = f'SELECT * FROM {nombre_tabla} WHERE fecha = {fecha} ORDER BY fecha DESC LIMIT 1'
-        #query_consulta = f'SELECT * FROM {nombre_tabla} WHERE id_region = 1 and id_categoria = 1'
+        query_consulta = f"SELECT * FROM {nombre_tabla} WHERE id_categoria = 1 AND fecha = '{fecha}' ORDER BY fecha"
         df_bdd = pd.read_sql(query_consulta,self.engine)
+        df_bdd = df_bdd.drop(['fecha', 'id_categoria', 'id_division', 'id_subdivision', 'valor'], axis=1)
+
+        # Multiplicar las columnas por 100
+        df_bdd['var_mensual'] *= 100
+        df_bdd['var_interanual'] *= 100
+        df_bdd['var_acumulada'] *= 100
+
+        # Convertir los valores a porcentaje
+        df_bdd['var_mensual'] = df_bdd['var_mensual'].apply(lambda x: f"{x:.2f}%")
+        df_bdd['var_interanual'] = df_bdd['var_interanual'].apply(lambda x: f"{x:.2f}%")
+        df_bdd['var_acumulada'] = df_bdd['var_acumulada'].apply(lambda x: f"{x:.2f}%")
+
+        # Obtener nombres de categorías
+        nombres = pd.read_sql("SELECT id_region, descripcion_region FROM identificador_regiones", self.engine)
+        diccionario_nombres = dict(zip(nombres['id_region'], nombres['descripcion_region']))
+
+        # Aplicar el mapeo de nombres a los datos
+        df_bdd['id_region'] = df_bdd['id_region'].map(diccionario_nombres)
+
 
         print("VARIACIONES POR REGION")
         print(df_bdd)
@@ -172,34 +268,69 @@ class InformeIPC:
 
         return df_bdd
     
-    # Objetivo: Leer de la base las variaciones desagregadas de la region del nea en la ultima fecha para tranformarlo en un df
+    # Objetivo: Leer de la base las variaciones mensuales, anuales y acumuladas de las 13 categorias de la region del nea en la ultima fecha para tranformarlo en un df
     def variaciones_nea(self, fecha):
         nombre_tabla = 'ipc_valores'
-        query_consulta = f'SELECT * FROM {nombre_tabla} WHERE fecha = {fecha} AND id_region = 5 ORDER BY fecha DESC LIMIT 1'
-        #query_consulta = f'SELECT * FROM {nombre_tabla} WHERE id_region = 1 and id_categoria = 1'
-        df_bdd = pd.read_sql(query_consulta,self.engine)
+        lista_subdivisiones = [1,2,14,17,20,25,27,30,35,37,41,42,44]
 
-        print("VARIAICONES NEA")
+        # Obtener datos filtrados
+        query_consulta = f""" SELECT fecha, id_region, id_categoria, id_subdivision, var_mensual, var_interanual, var_acumulada 
+        FROM {nombre_tabla} 
+        WHERE fecha = '{fecha}' AND id_region = 5 AND id_subdivision IN ({','.join(map(str, lista_subdivisiones))})
+        """
+        df_bdd = pd.read_sql(query_consulta, self.engine)
+
+        df_bdd = df_bdd.drop(['fecha', 'id_region', 'id_subdivision'], axis=1)
+
+        # Multiplicar las columnas por 100
+        df_bdd['var_mensual'] *= 100
+        df_bdd['var_interanual'] *= 100
+        df_bdd['var_acumulada'] *= 100
+
+        # Convertir los valores a porcentaje
+        df_bdd['var_mensual'] = df_bdd['var_mensual'].apply(lambda x: f"{x:.2f}%")
+        df_bdd['var_interanual'] = df_bdd['var_interanual'].apply(lambda x: f"{x:.2f}%")
+        df_bdd['var_acumulada'] = df_bdd['var_acumulada'].apply(lambda x: f"{x:.2f}%")
+
+
+        # Obtener nombres de categorías
+        nombres = pd.read_sql("SELECT id_categoria, nombre FROM ipc_categoria", self.engine)
+        diccionario_nombres = dict(zip(nombres['id_categoria'], nombres['nombre']))
+
+        # Aplicar el mapeo de nombres a los datos
+        df_bdd['id_categoria'] = df_bdd['id_categoria'].map(diccionario_nombres)
+
+
+        # Verificar los datos obtenidos
+        print("VARIACIONES NEA")
         print(df_bdd)
 
-        df_bdd = df_bdd.sort_values(by='var_mensual', ascending=[False])
+        # Ordenar el DataFrame por la columna 'var_mensual' de mayor a menor
+        df_bdd = df_bdd.sort_values(by='var_mensual', ascending=False)
 
         return df_bdd
 
     def main(self):
 
         variable_fecha_max = datetime.strptime('2024-07-01', '%Y-%m-%d')
-        
+        variable_fecha_max_str = variable_fecha_max.strftime('%Y-%m-%d %H:%M:%S')
+
         # Obtenemos variaciones nacionales de la ultima fecha
         var_mensual, var_interanual, var_acumulada = self.variaciones_nacion()
         
         # Obtenemos df de variaciones por region de la ultima fecha
-        df_var_region = self.variaciones_region(variable_fecha_max)
+        df_var_region = self.variaciones_region(variable_fecha_max_str)
 
         # Obtenemos df de variaciones extendido del nea de la ultima fecha
-        df_var_nea = self.variaciones_nea(variable_fecha_max)
+        df_var_nea = self.variaciones_nea(variable_fecha_max_str)
 
         # Enviamos el correo
-        self.enviar_correo(variable_fecha_max, var_mensual, var_acumulada, var_interanual, df_var_nea, df_var_region)
+        self.enviar_correo(variable_fecha_max_str, var_mensual, var_acumulada, var_interanual, df_var_nea, df_var_region)
 
 
+host = '54.94.131.196'
+user = 'estadistica'
+password = 'Estadistica2024!!'
+database = 'datalake_economico'
+
+InformeIPC(host,user,password,database).main()
